971422e969d9aed7826ef2edd2ba45d4726dc27b
jagasan dansena <jagasan.dansena@techo2>
2018-08-27 11:09:54 +0530

    fixed issue

--- src/main/java/com/tech/config/TenantInterceptor.java
+++ src/main/java/com/tech/config/TenantInterceptor.java
@@ -14,36 +14,37 @@ import com.tech.model.Tenant;
 import com.tech.repository.TenantRepository;
 
 @Component
-public class TenantInterceptor  extends HandlerInterceptorAdapter{
+public class TenantInterceptor extends HandlerInterceptorAdapter {
 
-	 Logger logger = LoggerFactory.getLogger(getClass());
-	    {
-	        logger.debug("Creating TenantInterceptor interceptor");
-	    }
+	Logger logger = LoggerFactory.getLogger(getClass());
+	{
+		logger.debug("Creating TenantInterceptor interceptor");
+	}
 
-	    @Autowired
-	    TenantRepository tenantRepository;
-	    
-	    @Override
-	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-	        String tenantUuid = request.getHeader("tenant-uuid");
-	        Tenant tenant = tenantRepository.findBySchemaName(tenantUuid);
-	        logger.debug("TenantInterceptor tenantUuid --------"
-	        		+tenantUuid);
-	        if(tenant==null)
-	        	throw new RuntimeException("Schema name can not be empty");
-	        else
-	        logger.debug("Set TenantContext:------- {}",tenant.getSchemaName());
-	        TenantContext.setTenantSchema(tenant.getSchemaName());
-	        return true;
-	    }
-	    
-	    @Override
-	    public void postHandle(
-	            HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
-	            throws Exception {
-	        logger.debug("Clear TenantContext: {}",TenantContext.getTenantSchema());
-	        TenantContext.setTenantSchema(null);
-	    }
-	
+	@Autowired
+	TenantRepository tenantRepository;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String tenantUuid = request.getHeader("tenant-uuid");
+		Tenant tenant = tenantRepository.findBySchemaName(tenantUuid);
+		String defSchma = "multiten";
+		if (tenant == null)
+			TenantContext.setTenantSchema(defSchma);
+		else {
+			if (tenant != null) {
+				TenantContext.setTenantSchema(tenant.getSchemaName());
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		logger.debug("Clear TenantContext: {}", TenantContext.getTenantSchema());
+		TenantContext.setTenantSchema(null);
+	}
+
 }
